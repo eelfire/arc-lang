db32fae1d84f3c7772e90b3cd2c33500ebe86954
Tirth Patel <tirth1093@gmail.com>
2024-01-26 22:06:33 +0530

    add missing tokens, add impl fn new() for Token

--- arc/src/token.rs
+++ arc/src/token.rs
@@ -12,6 +12,7 @@ pub enum Token {
     Let,
     Mut,
     Fx,
+    Tilde,
     Import,
     Pub,
     Mod,
@@ -26,6 +27,8 @@ pub enum Token {
     In,
     Continue,
     Break,
+    Match,
+    FatArrow,
 
     Return,
     Result,
@@ -86,11 +89,14 @@ pub enum Token {
     Semicolon,
     Dot,
     Colon,
+    Quotation,
+    Apostrophe,
 
     // Literals
-    NumType,
+    Num(i64),
     Char(char),
     String(String),
+    Bool(bool),
 
     // Identifiers
     Ident(String),
@@ -101,17 +107,88 @@ pub enum Token {
     EOF,
 }
 
-struct Int {
-    int32: i32,
-    int64: i64,
-}
+impl Token {
+    pub fn new(token: String) -> Self {
+        match token.as_str() {
+            "let" => Token::Let,
+            "mut" => Token::Mut,
+            "fx" => Token::Fx,
+            "~" => Token::Tilde,
+            "import" => Token::Import,
+            "pub" => Token::Pub,
+            "mod" => Token::Mod,
+            "super" => Token::Super,
+            "self" => Token::Itself,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "while" => Token::While,
+            "for" => Token::For,
+            "in" => Token::In,
+            "continue" => Token::Continue,
+            "break" => Token::Break,
+            "match" => Token::Match,
+            "=>" => Token::FatArrow,
+            "return" => Token::Return,
+            "result" => Token::Result,
+            "ok" => Token::Okay,
+            "err" => Token::Err,
+            "type" => Token::Type,
+            "struct" => Token::Struct,
+            "enum" => Token::Enum,
+            "impl " => Token::Impl,
 
-struct Float {
-    float32: f32,
-    float64: f64,
-}
+            // arithmetic operators
+            "+" => Token::Plus,
+            "-" => Token::Minus,
+            "*" => Token::Star,
+            "/" => Token::Slash,
+            "%" => Token::Percent,
 
-struct NumType {
-    int: Int,
-    float: Float,
+            // logical operators
+            "&&" => Token::LogicalAnd,
+            "||" => Token::LogicalOr,
+            "!" => Token::LogicalNot,
+            "&" => Token::BitwiseAnd,
+            "|" => Token::BitwiseOr,
+            "`" => Token::BitwiseNot,
+            "^" => Token::BitwiseXor,
+            "<<" => Token::BitwiseLeftShift,
+            ">>" => Token::BitwiseRightShift,
+
+            // comparison operators
+            "==" => Token::Eq,
+            "!=" => Token::Neq,
+            ">" => Token::Gt,
+            "<" => Token::Lt,
+            ">=" => Token::Gte,
+
+            // assignment operators
+            "=" => Token::Assign,
+            "+=" => Token::PlusAssign,
+            "-=" => Token::MinusAssign,
+            "*=" => Token::StarAssign,
+            "/=" => Token::SlashAssign,
+            "%=" => Token::PercentAssign,
+
+            // literals
+            "true" => Token::Bool(true),
+            "false" => Token::Bool(false),
+
+            _ => Token::Ident(token),
+        }
+    }
+
+    pub fn new_literal_num(token: String) -> Self {
+        // println!("token: {}", token);
+        // Token::Num(69)
+        Token::Num(token.parse::<i64>().unwrap())
+    }
+
+    pub fn new_literal_char(token: String) -> Self {
+        Token::Char(token.chars().nth(0).unwrap())
+    }
+
+    pub fn new_literal_string(token: String) -> Self {
+        Token::String(token)
+    }
 }
